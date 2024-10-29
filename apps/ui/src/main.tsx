4c16d9b4c08b0342e04f20f313b1d66943b2fd71
Sébastien Candau <sebastiencandau>
2024-10-29 13:49:19 +0100

    feat(ui): add router dom lib + init first home route example

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home/Home';
+
+const Main = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    {/* Ajoute d'autres routes ici si nécessaire */}
+  </Routes>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Main />
+    </Router>
+  </StrictMode>
+);
