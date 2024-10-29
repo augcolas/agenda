c998b4064a1cb3196155b22cd9756829f2da6fa0
Augustin Colas <augus.colas@orange.fr>
2024-10-29 11:25:28 +0100

    chore(api): adding eslint conf

---
+++ packages/eslint/nest.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+import tseslint from 'typescript-eslint';
+import baseConfigs from './base.js';
+
+export default tseslint.config(...baseConfigs, {
+  rules: {
+    // Both should be active, but needs refacto...
+    strictNullChecks: 0,
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'sonarjs/prefer-nullish-coalescing': 'off',
+  },
+});
