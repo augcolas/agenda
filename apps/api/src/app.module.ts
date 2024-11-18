ab6acd1d85ab45db740661b65b38c2c6f48b6d57
tdupuy <tom.dupuy@4sh.fr>
2024-11-18 01:43:21 +0100

    feat : add users to app - hashed password (salt is in .env file) - password field not sent back on API call

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../../.env',
+    }),
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number.parseInt(process.env.POSTGRES_PORT),
+      password: process.env.POSTGRES_PASSWORD,
+      username: process.env.POSTGRES_USER,
+      autoLoadEntities: true, // typeorm loads entities from this directory
+      database: process.env.POSTGRES_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
