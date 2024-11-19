7e2d247fd187612ab29e6ecab3faefeca194f686
Sébastien Candau <sebastiencandau>
2024-11-19 13:35:24 +0100

    hotfix(ui): fix conflict

--- apps/ui/src/contexts/AuthContext.tsx
+++ apps/ui/src/contexts/AuthContext.tsx
@@ -89,35 +89,6 @@ export const AuthProvider: React.FC<{ readonly children: ReactNode }> = ({ child
   );
 };
 
-export const useAuth = (): AuthContextType => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
-
-  const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-  export const AuthProvider: React.FC<{ readonly children: ReactNode }> = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [user, setUser] = useState<User | null>(null);
-
-    const login = (temporaryUser: User) => {
-      setUser(temporaryUser);
-      setIsAuthenticated(true);
-    };
-
-    const logout = () => {
-      setUser(null);
-      setIsAuthenticated(false);
-    };
-
-    return (
-      // eslint-disable-next-line sonarjs/jsx-no-constructed-context-values
-      <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
-        {children}
-      </AuthContext.Provider>
-    );
-  };
-
   export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
     if (!context) {
