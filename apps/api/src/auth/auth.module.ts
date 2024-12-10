b78626e3639c27ef040ec99276132bb87336cc59
tdupuy <tom.dupuy@4sh.fr>
2024-12-10 11:53:42 +0100

    fix (api) : env properties were not recognized when set in file, for auth module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'node:path';
 
@@ -7,6 +8,9 @@ import { AuthService } from './auth.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../../.env',
+    }),
     ClientsModule.register([
       {
         name: 'AUTHPROTO_PACKAGE',
