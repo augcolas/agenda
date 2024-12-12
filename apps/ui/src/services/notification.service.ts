34b3f38b8b16c7b7b891d473809f251a51c09738
Thrive <guillaumemengelatte@hotmail.fr>
2024-12-12 09:11:27 +0100

    fix(ui) : service base url viariable name

--- apps/ui/src/services/notification.service.ts
+++ apps/ui/src/services/notification.service.ts
@@ -6,13 +6,13 @@ import {
 
 import api from "./api.service";
 
-const API_BASE_URL = "/notifications";
+const NOTIFICATION_BASE_URL = "/notifications";
 
 export const NotificationService = {
   async getAllUserNotifications(
     userId: number,
   ): Promise<GetNotificationListResponse["notifications"]> {
-    const response = await api.get(`${API_BASE_URL}/user/${userId}`);
+    const response = await api.get(`${NOTIFICATION_BASE_URL}/user/${userId}`);
     return response.data.notifications;
   },
 
@@ -20,16 +20,16 @@ export const NotificationService = {
     notificationId: string,
     userId: number,
   ): Promise<void> {
-    await api.delete(`${API_BASE_URL}/${userId}/${notificationId}`);
+    await api.delete(`${NOTIFICATION_BASE_URL}/${userId}/${notificationId}`);
   },
 
   async updateNotifications(notifications: UpdateNotificationListRequest) {
-    const response = await api.patch(API_BASE_URL, notifications);
+    const response = await api.patch(NOTIFICATION_BASE_URL, notifications);
     return response.data;
   },
 
   async addNotification(notifications: AddNotificationListRequest) {
-    const response = await api.post(API_BASE_URL, notifications);
+    const response = await api.post(NOTIFICATION_BASE_URL, notifications);
     return response.data;
   },
 };
